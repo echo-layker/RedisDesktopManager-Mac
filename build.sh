b0d6070509c3009a2c50916164bd31778c3de5de
zhangdingshui <zhangdingshui@changingedu.com>
2020-04-12 22:38:28 +0800

    add build.sh

--- build.sh
+++ build.sh
@@ -1,44 +1,32 @@
 #!/bin/bash
 
 SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
-
-echo '=======================Clone RDM repository======================='
-rm -rf rdm
-git clone --recursive https://github.com/uglide/RedisDesktopManager.git rdm
-echo "=======================Switch to latest tag: ${TAG}===================="
 cd $SHELL_FOLDER/rdm
-TAG=$(git describe --tags `git rev-list --tags --max-count=1`)
-echo 'switch to latest tag: '$TAG
-cd $SHELL_FOLDER/rdm
-git checkout -b $TAG $TAG
-echo '=======================Build CrashReporter======================='
-cd $SHELL_FOLDER/rdm/3rdparty/crashreporter
-# errors on latest version of crashrepoter, build version 7ec6f00 instead.
-git checkout 7ec6f00
-qmake DESTDIR=./bin CONFIG-=debug
-make -s -j 8
-mkdir -p $SHELL_FOLDER/rdm/bin/osx/release
-mv $SHELL_FOLDER/rdm/3rdparty/crashreporter/bin/crashreporter $SHELL_FOLDER/rdm/bin/osx/release
-
-
 echo '=======================Modify RDM version======================='
-cd $SHELL_FOLDER/rdm/src
+cd $SHELL_FOLDER/rdm/src/resources
 echo 'copy Info.plist'
-cp resources/Info.plist.sample resources/Info.plist
-echo 'modify rdm.pro'
-sed -i '' "s/2019.3/$TAG/g" rdm.pro
-sed -i '' 's/debug: CONFIG-=app_bundle/#debug: CONFIG-=app_bundle/g' rdm.pro
+cp Info.plist.sample Info.plist
+echo 'modify Info.plist'
+sed -i "s/0.0.0/$TAG/g" Info.plist
 
+echo '=======================Install Python requirements======================='
+mkdir -p $SHELL_FOLDER/rdm/bin/osx/release
+cd $SHELL_FOLDER/rdm/bin/osx/release
+touch setup.cfg
+/bin/cat <<EOM >setup.cfg
+[install]
+prefix=
+EOM
+pip3 install -t ./ -r ../../../src/py/requirements.txt --upgrade
 
 echo "=======================Build RDM ${TAG}======================="
 cd $SHELL_FOLDER/rdm/src
-./configure
-qmake CONFIG-=debug
+qmake CONFIG-=debug CONFIG+=sdk_no_version_check
 make -s -j 8
 
 echo "=======================Copy QT Framework(optional)======================="
 cd $SHELL_FOLDER/rdm/bin/osx/release
-macdeployqt rdm.app -qmldir=../../../src/qml
+macdeployqt Redis\ Desktop\ Manager.app -qmldir=../../../src/qml -dmg
 
 echo "=======================SUCCESS======================="
-echo 'App file is:'$SHELL_FOLDER/rdm/bin/osx/release/rdm.app
+echo 'App file is:'$SHELL_FOLDER/rdm/bin/osx/release/Redis\ Desktop\ Manager.app.app
